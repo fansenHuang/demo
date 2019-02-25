e64b0dc79791db0ef30ec1489e16f8c646834d5a
chenjinming <chenjinming>
2019-02-25 13:53:29 +0800

    测试

--- src/main/java/com/core/test/test.java
+++ src/main/java/com/core/test/test.java
@@ -5,4 +5,11 @@ public class test {
     public static void main(String[] args){
         System.out.println("测试");
     }
+
+    public String aaa(){
+        System.out.println("测试项目代码合并");
+        return "";
+    }
+
+
 }
