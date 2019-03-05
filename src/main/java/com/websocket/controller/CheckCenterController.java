ed0fcec0f2b276bf20ae0c05f44603ce6acb7ebd
fansenHuang <1763823747@qq.com>
2019-03-05 16:46:19 +0800

    websocket

---
+++ src/main/java/com/websocket/controller/CheckCenterController.java
@@ -0,0 +1,39 @@
+package com.websocket.controller;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.websocket.server.ApiReturnObject;
+import com.websocket.server.ApiReturnUtil;
+import com.websocket.server.WebSocketServer;
+
+@Controller
+@RequestMapping("/checkcenter")
+public class CheckCenterController {
+	// 页面请求
+	@GetMapping("/socket/{cid}")
+	public ModelAndView socket(@PathVariable String cid) {
+		ModelAndView mav = new ModelAndView("/notice/socket");
+		mav.addObject("cid", cid);
+		return mav;
+	}
+
+	// 送数据接口
+	@ResponseBody
+	@RequestMapping("/socket/push/{cid}")
+	public ApiReturnObject pushToWeb(@PathVariable String cid, String message) {
+		try {
+			WebSocketServer.sendInfo(message, cid);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return ApiReturnUtil.error(cid + "#" + e.getMessage());
+		}
+		return ApiReturnUtil.success(cid);
+	}
+}
